9aa9d95fe4d5048849a1a8e8e6d7a9b730ff1e46
odipio <dipuccioo@gmail.com>
2022-04-04 15:59:15 -0400

    new login page

---
+++ js/login.js
@@ -0,0 +1,14 @@
+﻿documen.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", () => {
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", () => {
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+});
+
