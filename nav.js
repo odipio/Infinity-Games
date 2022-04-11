924b3f3cf8bce86c19b62002edd6c420670be331
hysinggw <hysinggw@miamioh.edu>
2022-04-11 18:29:30 -0400

    navbar is now in nav.js and is called in each page

---
+++ nav.js
@@ -0,0 +1,19 @@
+var navbar = '<nav><ul><li><img src="images/logo2.png" width="75" height="44" /></li><li><a href="homePage.html">Home</a></li><li><a href="request.html">Request a Game</a></li><li><a href="browse.html">Browse</a></li><lir><input type="text" id="search" placeholder="Search" /><a href=""><button style="margin-right: 25px; margin-top: 13px" type="button"><img src="https://static.thenounproject.com/png/424965-200.png" width="10"height="10"/></button></a></lir><li><a href="login.html">Login</a></li></ul></nav>';
+document.body.insertAdjacentHTML("afterbegin", navbar);
+
+/* Generic User Navbar
+  <nav>
+        <ul>
+            <li><img src="images/logo2.png" width="75" height="44"></li>
+            <li><a href="homePage.html">Home</a></li>
+            <li><a href="request.html">Request a Game</a></li>
+            <li><a href="browse.html">Browse</a></li>
+            <lir>
+              <input type="text" id="search" placeholder="Search">
+              <a href=""><button style="margin-right:25px; margin-top: 13px;" type="button">
+                <img src="https://static.thenounproject.com/png/424965-200.png" width="10" height="10"></button>
+              </a>
+            </lir>
+            <li><a href="login.html">Login</a></li>
+        </ul>
+    </nav> */
